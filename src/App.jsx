fa56ac2f708bfea8b51f02984977c51504453fba
hasan00-lab <enghasan313@gmail.com>
2026-09-10 02:50:15 +0300

    Add exam and book PDFs and fix production API

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@
 import React, { useEffect, useMemo, useState } from "react";
 import "./styles.css";
 
-const API = "http://localhost:5000/api";
-const SERVER = "http://localhost:5000";
+const API = "/api";
+const SERVER = "";
 
 const branchInfo = {
   LS: {
